e6c7c25aab7ea8af169b22d6f21585fbdb696116
Xavier Au <xavier.au@anacreation.com>
2017-04-28 21:17:25 +0800

    update multiple fill in the blanks grader

--- src/Graders/MultipleFillInBlanksGrader.php
+++ src/Graders/MultipleFillInBlanksGrader.php
@@ -14,48 +14,101 @@ use Anacreation\Etvtest\Models\Question;
 class MultipleFillInBlanksGrader implements GraderInterface
 {
     use GraderTrait;
+
     /**
      * @param \Anacreation\Etvtest\Models\Question $question
-     * @param array         $answer
+     * @param array                                $answer
      * @return array
      */
     public function grade(Question $question, array $answers) {
 
-        $answerObject =  $question->answer;
+        $answerObject = $question->answer;
         $answerStringArray = [];
 
-        if($this->isEmptyAnswer($answers)) return [false, $answerObject->content];
-
-
-        foreach ($answerObject->content as $choiceId){
+        foreach ($answerObject->content as $choiceId) {
             $answerStringArray[] = Choice::findOrFail($choiceId)->content;
         }
 
-        if($answerObject->is_required_all and $answerObject->is_ordered){
-            $correct = true;
-            foreach ($answerStringArray as $index=>$answerString){
-                if($answers[$index] != $answerString) $correct = false;
-            }
-            return [$correct, $answerStringArray];
+        if ($this->isEmptyAnswer($answers)) {
+            return [false, $answerStringArray];
         }
 
-        if($answerObject->is_required_all and !$answerObject->is_ordered){
+        return [$this->checkAnswer($answers, $answerObject, $answerStringArray), $answerStringArray];
+
+    }
+
+    /**
+     * @param array $answers
+     * @param       $answerObject
+     * @param       $answerStringArray
+     * @return array
+     */
+    private function checkAnswer(array $answers, $answerObject, $answerStringArray): bool {
+
+        if ($this->requiredAllAndInOrdered($answerObject)) {
+            $correct = true;
+            foreach ($answerStringArray as $index => $answerString) {
+                if ($answers[$index] != $answerString) {
+                    $correct = false;
+                }
+            }
+
+            return $correct;
+        }
+
+        if ($this->requiredAllAndNotInOrder($answerObject)) {
             $result = array_diff($answerStringArray, $answers);
-            return [$result == 0, $answerStringArray];
+
+            return $result == 0;
         }
 
-        if(!$answerObject->is_required_all and $answerObject->is_ordered){
+        if ($this->notRequiredAllAndInOrdered($answerObject)) {
             $correct = true;
-            foreach ($answers as $index=>$answer){
-                if($answerStringArray[$index] != $answer) $correct = false;
+            foreach ($answers as $index => $answer) {
+                if ($answerStringArray[$index] != $answer) {
+                    $correct = false;
+                }
             }
-            return [$correct, $answerStringArray];
+
+            return $correct;
         }
 
-        if(!$answerObject->is_required_all and !$answerObject->is_ordered){
+        if ($this->notRequiredAllAndNotInOrdered($answerObject)) {
             $result = array_intersect($answerStringArray, $answers);
-            return [$result > 0, $answerStringArray];
-        }
 
+            return $result > 0;
+        }
+    }
+
+    /**
+     * @param $answerObject
+     * @return bool
+     */
+    private function requiredAllAndInOrdered($answerObject): bool {
+        return $answerObject->is_required_all and $answerObject->is_ordered;
+    }
+
+    /**
+     * @param $answerObject
+     * @return bool
+     */
+    private function requiredAllAndNotInOrder($answerObject): bool {
+        return $answerObject->is_required_all and !$answerObject->is_ordered;
+    }
+
+    /**
+     * @param $answerObject
+     * @return bool
+     */
+    private function notRequiredAllAndInOrdered($answerObject): bool {
+        return !$answerObject->is_required_all and $answerObject->is_ordered;
+    }
+
+    /**
+     * @param $answerObject
+     * @return bool
+     */
+    private function notRequiredAllAndNotInOrdered($answerObject): bool {
+        return !$answerObject->is_required_all and !$answerObject->is_ordered;
     }
 }
