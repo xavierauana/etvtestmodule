be549ba0730160239449d02f0fde6fcb85a51797
Xavier Au <xavier.au@anacreation.com>
2017-05-31 22:17:27 +0800

    update multiple mc grader when require all

--- src/Graders/MultipleMultipleChoiceGrader.php
+++ src/Graders/MultipleMultipleChoiceGrader.php
@@ -9,29 +9,37 @@ namespace Anacreation\Etvtest\Graders;
 
 
 use Anacreation\Etvtest\Models\Question;
-use Illuminate\Pagination\Paginator;
 
 class MultipleMultipleChoiceGrader implements GraderInterface
 {
     use GraderTrait;
+
     /**
      * @param \Anacreation\Etvtest\Models\Question $question
-     * @param array         $answer
+     * @param array                                $answer
      * @return array
      */
     public function grade(Question $question, array $answer) {
         $correct = true;
-        $answerObject =  $question->answer;
+        $answerObject = $question->answer;
 
-        if($this->isEmptyAnswer($answer)) return [false, $answerObject->content];
+        if ($this->isEmptyAnswer($answer)) {
+            return [false, $answerObject->content];
+        }
 
-        if($answerObject->is_required_all){
-            $result = array_diff($answer, $answerObject->content);
-            if(count($result) > 0) $correct = false;;
+        if ($answerObject->is_required_all) {
+            $result = array_diff($answerObject->content, $answer);
+            if (count($result) > 0) {
+                $correct = false;
+            };
+
             return [$correct, $answerObject->content];
-        }else{
+        } else {
             $result = array_intersect($answerObject->content, $answer);
-            if(count($result) <= 0) $correct = false;;
+            if (count($result) <= 0) {
+                $correct = false;
+            };
+
             return [$correct, $answerObject->content];
         }
 
